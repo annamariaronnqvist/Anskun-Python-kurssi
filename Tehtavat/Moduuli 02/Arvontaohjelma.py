9c29ad408839fc109eb2298d258885b847ec6d91
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-09-02 10:40:05 +0300

    Module 2 commits git/commit: kaikki tehtävät paitsi viimeinen, leiviskähommassa pitää vielä erotella kg ja g

---
+++ Tehtavat/Moduuli 02/Arvontaohjelma.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+print("Arvotaan seuraavaksi kolminumeroinen koodi (0-6)")
+
+print("Arotaan seuraavaksi nelinumeroinen koodi (1-6)")
+
+
+
+
+#Kirjoita ohjelma, joka arpoo ja tulostaa
+# kaksi erilaista numerolukon koodia:
+#kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
+#nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
+#Vihje: tutustu random.randint()-funktion käyttöön.
+
