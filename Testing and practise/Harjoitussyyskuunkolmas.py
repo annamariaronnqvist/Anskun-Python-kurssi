6cc9745cbdee6dc3249ca48dc7c82b112f0c2fe6
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-09-03 21:53:08 +0300

    Module 2 commits git/commit: tässä myös viimeinen tehtävä, eli lukkokoodi

---
+++ Testing and practise/Harjoitussyyskuunkolmas.py
@@ -0,0 +1,7 @@
+kokonaisuluku = int(input("Anna minulle kokonaisuluku"))
+
+if kokonaisuluku < 0:
+    print(f"luvun itseisarvo on {kokonaisuluku} *-1")
+else:
+    print(f"luvun itseisarvo on {kokonaisuluku}.")
+
--- Testing and practise/laskin.py
+++ Testing and practise/laskin.py
@@ -1,5 +1,7 @@
-a = float(input("anna ensimmäinen luku:")
-b = float(input("Anna toinen luku:")
+import math
+
+a = int(input("anna ensimmäinen luku:"))
+b = int(input("Anna toinen luku:"))
 print("yhteenlasku", a + b)
 print("vähennyslasku", a - b)
 print("kertolasku", a * b)
@@ -16,6 +18,8 @@ print(a // b)
 
 height = 155
 weight = 62
+BMI = (weight / height ** 2)*100
 
+print(BMI)
 #the BMI is body mass / square of height
 #tee laskin loppuun
