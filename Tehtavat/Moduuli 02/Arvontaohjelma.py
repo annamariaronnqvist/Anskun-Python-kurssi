07efe0b6652e02a9a3d83c57c6bcb2fcae260e0d
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-09-03 07:17:15 +0300

    Module 2 commits git/commit: tässä myös viimeinen tehtävä, eli lukkokoodi

--- Tehtavat/Moduuli 02/Arvontaohjelma.py
+++ Tehtavat/Moduuli 02/Arvontaohjelma.py
@@ -1,10 +1,30 @@
 import math
 import random
 
+#rnumber = [random.randint(number1, number2) for x in range(times)]
+
 print("Arvotaan seuraavaksi kolminumeroinen koodi (0-6)")
+ekakoodi = random.randint(0,6)
+tokakoodi = random.randint(0,6)
+kolmaskoodi = random.randint(0,6)
 
-print("Arotaan seuraavaksi nelinumeroinen koodi (1-6)")
+print("lukkokoodi alla:")
+print(ekakoodi, tokakoodi, kolmaskoodi)
 
+#tähän pitäisi saada jokin looppi, mutta tällä saadaan koodi
+
+print("Arvotaan seuraavaksi nelinumeroinen koodi (1-6)")
+nelieka = random.randint(1,6)
+nelitoka = random.randint(1,6)
+nelikolmas = random.randint(1,6)
+nelinelos = random.randint(1,6)
+
+print("lukkokoodi alla:")
+print(nelieka, nelitoka, nelikolmas, nelinelos)
+
+#eli jotta tämä olisi nätti, minun tulisi saada tuo random ajamaan itsensä
+#uudelleen ja sitten varmistaa, että se arpoo uuden numeron, joka voi olla myös
+#sama kuin edellinen.
 
 
 
