5cada8a654068b6920504beb6d8d6d8f6ed556a4
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-09-27 00:26:07 +0300

    Module 4, tehtävä 1 puolikas

--- Tehtavat/Moduuli 04/Lukukysely.py
+++ Tehtavat/Moduuli 04/Lukukysely.py
@@ -14,7 +14,7 @@ while (number_given > 1 or number_given < 1):
 #print ("suurin luku oli: 10")
 #, "pienin luku oli:" min_str)
 
-print ("Leikki loppui tähän")
+print ("Leikki loppui tähän, nyt pitää keksiä miten min ja max käyttäytyy")
 
 #print(min_number and max_number)
 
