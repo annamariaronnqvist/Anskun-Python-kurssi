1a67b0b5d9d859a79561eb872afc39d8ae7b8594
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-09-27 00:39:10 +0300

    Module 4, tehtävä 2 kokonaan

--- Tehtavat/Moduuli 04/kolmijakotulostin.py
+++ Tehtavat/Moduuli 04/kolmijakotulostin.py
@@ -1,10 +1,15 @@
 #Kirjoita while-toistorakennetta käyttävä ohjelma, joka
 # tulostaa kolmella jaolliset luvut väliltä 1..1000.
 
-number = 1
-Max_number = 1000
-while number % 3 and number <= 1000:
-    print ("kolmella jaollinen:" number)
-    number += 1
+number = 0
+Max_number = 998
+
+while number < Max_number:
+    number += 3
+    print(number)
+
+    #= 0 and number <= 1000:
+
+
 
 print ("nyt on kaikki kolmijaolliset tulostettu")
