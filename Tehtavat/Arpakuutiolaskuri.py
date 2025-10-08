8de8e9bb9bec64313f1135a9028ebd2ae4ee61ec
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-10-08 23:39:54 +0300

    Module 5, tehtävä 1 puoliksi

---
+++ Tehtavat/Arpakuutiolaskuri.py
@@ -0,0 +1,17 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+#Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
+#Käytä for-toistorakennetta.
+
+import random
+kuutioluku = int(input("kuinka monta arpaa haluat"))
+kuutiosilmat = random.randint(1, 6)
+count= 1
+
+for numbers in kuutiosilmat:
+
+    while kuutioluku < kuutioluku: #or kuutioluku == kuutioluku:
+        print (sum(kuutiosilmat))
+        #for kuutioluku:
+        count +1
+    else:
+        print (sum(kuutiosilmat))
