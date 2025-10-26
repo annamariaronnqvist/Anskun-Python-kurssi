6d56e1ea6bb6d7825310b41ae6e5b29827c890fc
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-10-27 00:31:22 +0200

    Module 6, harkkaa

---
+++ Testing and practise/funktiotestit.py
@@ -0,0 +1,20 @@
+def tervehdi():
+    print("Moi!")
+    return
+
+print("onpas kiva päivä, eikö?")
+tervehdi()
+print("tämän jälkeen on aika siirtyä seuraavaan settiin")
+
+def tervehdi(kerrat):
+    for i in range(kerrat):
+        print("Hyvää päivää" + str(i+1) + ". kerran")
+
+    return
+print ("Päivää alkaa tervehdyksillä.")
+#tämä voisi olla myös muuttuja, jota kutsutaan, jota pyydetään käyttäjältä
+tervehdi(5)
+print("tervehditään lisää")
+tervehdi(2)
+
+def (vaihda())
