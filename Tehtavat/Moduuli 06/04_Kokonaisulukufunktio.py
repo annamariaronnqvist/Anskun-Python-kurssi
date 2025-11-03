b96b6624c403f969dfba77af3bad7f51ef065c76
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-11-04 00:08:51 +0200

    Module 6, tehtävä 4. Funktio lähtee rakentamaan listaa. Sitten feilaa.

--- Tehtavat/Moduuli 06/04_Kokonaisulukufunktio.py
+++ Tehtavat/Moduuli 06/04_Kokonaisulukufunktio.py
@@ -2,3 +2,19 @@
 # Ohjelma palauttaa listassa olevien lukujen summan. Kirjoita testausta
 # varten pääohjelma, jossa luot listan,
 # kutsut funktiota ja tulostat sen palauttaman summan.
+
+kokonaislukulista = []
+
+def lukupalauttaja():
+    kokonaisluku = int(input("Anna minulle kokonaisuluku, enter lopettaa "))
+
+    while kokonaisluku != "":
+        kokonaislukulista.append(kokonaisluku)
+        kokonaisluku = int(input("Anna minulle kokonaisuluku, enter lopettaa "))
+
+    else:
+        print(sum(kokonaislukulista))
+
+    return
+
+lukupalauttaja()
