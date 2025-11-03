142c024abae2db85cd3752d480f7268f2167c036
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-11-03 23:02:24 +0200

    Module 6, tehtävä 1. Nyt arpoo numeron ja tulostaa, mutta ei funktiota käyttäen

--- Tehtavat/Moduuli 06/03_litramuunnin_bensa.py
+++ Tehtavat/Moduuli 06/03_litramuunnin_bensa.py
@@ -4,3 +4,16 @@
 # ja muuntaa sen litroiksi. Muunnos on tehtävä aliohjelmaa hyödyntäen.
 # Muuntamista jatketaan siihen saakka, kunnes käyttäjä syöttää negatiivisen gallonamäärän.
 # Yksi gallona on 3,785 litraa.
+
+bensat = int(input("anna bensamäärä "))
+def galloona(bensat):
+
+    while bensat > 0:
+        print(f"{bensat * 3.785:.2f}")
+        break
+    else:
+        print("Kiitos kun käytit bensamuunninta")
+
+        return
+
+galloona(bensat)
