c63c35461638331ac014f1fd99470ffee170bd71
Anna-Maria Rönnqvist <anna-maria.ronnqvist@metropolia.fi>
2025-11-04 00:29:40 +0200

    Module 6, tehtävä 4. Funktio lähtee rakentamaan listaa. Sitten feilaa. 5 on kesken

--- Tehtavat/Moduuli 06/05_Kokonaislukufunktio_ei_pariton.py
+++ Tehtavat/Moduuli 06/05_Kokonaislukufunktio_ei_pariton.py
@@ -4,3 +4,26 @@
 # parittomat luvut. Kirjoita testausta varten pääohjelma, jossa luot
 # listan, kutsut funktiota ja tulostat sen jälkeen sekä alkuperäisen
 # että karsitun listan.
+
+kokonaislukulista = []
+
+#määritellään funktio
+
+def parillinenlukupalauttaja(kokonaislukulista):
+    kokonaisluku = int(input("Anna minulle kokonaisuluku, enter lopettaa "))
+#annetaan ehdot, lisätään lukuja käyttäjän haluaman mukaisesti
+
+    while kokonaisluku != "":
+    #lisäillään listaan käytääjän määrittelemät luvut
+        if kokonaisluku % 2 == 0:
+            kokonaislukulista.append(kokonaisluku)
+            print(sum(kokonaislukulista))
+        else:
+            kokonaisluku = int(input("Anna minulle kokonaisuluku, enter lopettaa "))
+
+    else:
+        print(sum(kokonaislukulista))
+
+    return
+
+parillinenlukupalauttaja(kokonaislukulista)
